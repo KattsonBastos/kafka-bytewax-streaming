2ec0157f0ef0e41a7306335feaec65a6adf1b9f1
Kattson Bastos <kattsonbastos@gmail.com>
2024-06-16 21:53:38 +0000

    feat: add kafka commands

--- src/kafka/create-topics.sh
+++ src/kafka/create-topics.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-kafka-topics --create --topic=rides --bootstrap-server=localhost:9092 --partitions=3 --replication-factor=1
+kafka-topics --create --topic=input-raw-rides --bootstrap-server=localhost:9092 --partitions=3 --replication-factor=1
 
 kafka-topics --create --topic=output-bytewax-enriched-rides --bootstrap-server=localhost:9092 --partitions=3  --replication-factor=1
 
-kafka-console-consumer --bootstrap-server=localhost:9092 --topic=rides-refined
+kafka-console-consumer --bootstrap-server=localhost:9092 --topic=output-bytewax-enriched-rides
