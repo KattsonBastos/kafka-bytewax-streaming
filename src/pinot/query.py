2b23a688a735ae9acae42f7b413bc27e2de82157
Kattson Bastos <kattsonbastos@gmail.com>
2024-06-16 21:53:58 +0000

    feat: add pinot jsons

---
+++ src/pinot/query.py
@@ -0,0 +1,24 @@
+# here I'm just playing with some queries. If you want to try your,
+# just replace the value of the `PINOT_QUERY` variable below
+
+import time
+from pinotdb import connect
+
+conn = connect(host='localhost', port=8099, path='/query/sql', scheme='http')
+curs = conn.cursor()
+
+PINOT_QUERY = """
+    SELECT
+        COUNT(*) as processing_time_converted
+    FROM
+    sch_output_bytewax_enriched_rides_events
+    WHERE
+        DATETIMECONVERT(processing_time, '1:MILLISECONDS:SIMPLE_DATE_FORMAT:yyyy-MM-dd HH:mm:ss.SSSSSS', '1:MILLISECONDS:EPOCH', '1:MILLISECONDS') >= ago('PT1S')
+"""
+
+while True:
+    curs.execute(PINOT_QUERY)
+    for row in curs:
+        print(row)
+    
+    time.sleep(1)
--- src/pinot/run.sh
+++ src/pinot/run.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
+### -- this script copies ./pinot-*.json to the pinot-controller container and then creates table and schema
 
 echo "Copying files.."
 TABLE_PATH=/tmp/pinot-table.json
 SCHEMA_PATH=/tmp/pinot-schema.json
 
-docker cp pinot-table.json pinot-controller:/tmp/pinot-table.json
-docker cp pinot-schema.json pinot-controller:/tmp/pinot-schema.json
-docker cp pinot-tenant.json pinot-controller:/tmp/pinot-tenant.json
+docker cp pinot-table.json pinot-controller:$TABLE_PATH
+docker cp pinot-schema.json pinot-controller:$SCHEMA_PATH
 
 echo "Creating schema.."
-# docker exec -it pinot-controller /opt/pinot/bin/pinot-admin.sh AddSchema \
-#   -schemaFile /tmp/pinot-schema.json \
-#   -controllerHost pinot-controller \
-#   -controllerPort 9000 -exec
-
-echo "Creating table.."
-docker exec -it pinot-controller /opt/pinot/bin/pinot-admin.sh AddTable \
-  -schemaFile /tmp/pinot-schema.json \
-  -tableConfigFile /tmp/pinot-table.json \
+docker exec -it pinot-controller /opt/pinot/bin/pinot-admin.sh AddSchema \
+  -schemaFile $SCHEMA_PATH \
   -controllerHost pinot-controller \
   -controllerPort 9000 -exec
 
-# docker exec -it pinot-controller /opt/pinot/bin/pinot-admin.sh AddTable \
-# -tableConfigFile /tmp/pinot-table.json -exec
-
-
-
-# /opt/pinot/bin/pinot-admin.sh AddSchema   -schemaFile pinot-schema.json -exec
+echo "Creating table.."
+docker exec -it pinot-controller /opt/pinot/bin/pinot-admin.sh AddTable \
+  -schemaFile $SCHEMA_PATH \
+  -tableConfigFile $TABLE_PATH \
+  -controllerHost pinot-controller \
+  -controllerPort 9000 -exec
